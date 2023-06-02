332ebf0fddd34e91b03a832cfafaa826306558f9
André Martins <aanm90@gmail.com>
2023-06-02 16:21:46 +0200

    support multiple junit files
    Signed-off-by: André Martins <aanm90@gmail.com>

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -3,15 +3,17 @@
 junit_directory="${1}"
 
 for filename in "${junit_directory}"/*.xml; do
+  echo ""                                                               >> "$GITHUB_STEP_SUMMARY";
   junit2md -i "${filename}" -v=true -s=false -p=false                   >> "$GITHUB_STEP_SUMMARY";
 done
 echo ""                                                                 >> "$GITHUB_STEP_SUMMARY"
-echo "## Summary of all tests"                                                                 >> "$GITHUB_STEP_SUMMARY"
+echo "## Summary of all tests"                                          >> "$GITHUB_STEP_SUMMARY"
 echo ""                                                                 >> "$GITHUB_STEP_SUMMARY"
 echo "<details>"                                                        >> "$GITHUB_STEP_SUMMARY"
-echo "  <summary>Click here to see</summary>" >> "$GITHUB_STEP_SUMMARY"
-echo ""                                                                 >> "$GITHUB_STEP_SUMMARY"
+echo "  <summary>Click here to see</summary>"                           >> "$GITHUB_STEP_SUMMARY"
 for filename in "${junit_directory}"/*.xml; do
+  echo ""                                                               >> "$GITHUB_STEP_SUMMARY";
+  echo "#### ${filename}"                                               >> "$GITHUB_STEP_SUMMARY";
   junit2md -i "${filename}" -s=false                                    >> "$GITHUB_STEP_SUMMARY";
 done
 echo "</details>"                                                       >> "$GITHUB_STEP_SUMMARY"
